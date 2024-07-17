e0c0a4e1c2e6cb494fc64be0783bcbd4118364c3
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2024-07-17 21:55:27 +0900

    ✨ feat : 업로드 버튼 스타일 수정, 업로드 가능 이미지 수 제한

--- dojang-crush/src/pages/Upload.js
+++ dojang-crush/src/pages/Upload.js
@@ -1,50 +1,144 @@
+import { useState, useRef } from "react";
 import TopBarUpload from "../components/TopBarUpload";
 import styled from "styled-components";
+import { ReactComponent as UploadButton } from "../assets/ui/photo_upload.svg";
 
 const UploadPage = () => {
-  return (
-    <UploadWrapper>
-      <TopBarUpload text="Upload" />
-      <ContentsWrapper>
-        <Tags />
-        <Contents />
-        <IMGBox type="file" accept="image/*" />
-      </ContentsWrapper>
-    </UploadWrapper>
-  );
+    const [images, setImages] = useState([]);
+    const fileInputRef = useRef(null);
+
+    const handleImageUpload = (e) => {
+        const files = Array.from(e.target.files);
+        const newImages = [];
+
+        files.forEach((file) => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                newImages.push(reader.result);
+                if (newImages.length === files.length) {
+                    setImages((prevImages) =>
+                        [...prevImages, ...newImages].slice(0, 4)
+                    );
+                }
+            };
+            reader.readAsDataURL(file);
+        });
+    };
+
+    const handleRemoveImage = (index) => {
+        setImages(images.filter((_, i) => i !== index));
+    };
+
+    const handleClick = () => {
+        fileInputRef.current.click();
+    };
+
+    return (
+        <UploadWrapper>
+            <TopBarUpload text="Upload" />
+            <ContentsWrapper>
+                <Tags />
+                <Contents />
+                <ImageContainer>
+                    {images.length < 4 && (
+                        <>
+                            <ImageUploadButton onClick={handleClick} />
+                            <IMGBox
+                                type="file"
+                                accept="image/*"
+                                multiple
+                                onChange={handleImageUpload}
+                                ref={fileInputRef}
+                            />
+                        </>
+                    )}
+                    {images.map((src, index) => (
+                        <ImagePreviewWrapper key={index}>
+                            <RemoveButton
+                                onClick={() => handleRemoveImage(index)}
+                            >
+                                X
+                            </RemoveButton>
+                            <ImagePreview
+                                src={src}
+                                alt={`uploaded image ${index + 1}`}
+                            />
+                        </ImagePreviewWrapper>
+                    ))}
+                </ImageContainer>
+            </ContentsWrapper>
+        </UploadWrapper>
+    );
 };
 
 export default UploadPage;
 
 const UploadWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
 `;
 
 const ContentsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3vh;
+    display: flex;
+    flex-direction: column;
+    gap: 3vh;
 `;
 
 const Tags = styled.input`
-  width: 90vw;
-  padding: 3vw;
-  border: none;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
-  font-size: 2.5rem;
+    width: 90vw;
+    padding: 3vw;
+    border: none;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+    font-size: 2.5rem;
 `;
 
 const Contents = styled.textarea`
-  width: 90vw;
-  height: 40vh;
-  padding: 3vw;
-  border: none;
-  font-size: 2rem;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+    width: 90vw;
+    height: 40vh;
+    padding: 3vw;
+    border: none;
+    font-size: 2rem;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
 `;
 
+const ImageContainer = styled.div`
+    display: flex;
+    align-items: start;
+    gap: 2vw;
+`;
+
+const ImageUploadButton = styled(UploadButton)`
+    cursor: pointer;
+`;
 const IMGBox = styled.input`
-  border: none;
+    display: none;
+`;
+const ImagePreviewWrapper = styled.div`
+    position: relative;
+    display: inline-block;
+`;
+
+const ImagePreview = styled.img`
+    max-width: 200px;
+    max-height: 200px;
+    object-fit: cover;
+    border-radius: 5px;
+`;
+
+const RemoveButton = styled.button`
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    background: rgba(255, 0, 0, 0.7);
+    color: white;
+    border: none;
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    font-size: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
