9329ce5390962587d70ef55055cb16fff19a6f8e
yyeonzu <tooma22@naver.com>
2024-08-09 00:42:22 +0900

    ✨ 캘린더 페이지 api 연결 & 퍼블리싱

---
+++ dojang-crush/src/components/calendar/ModalContent.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    gap: 2rem;
+    font-size: 1.2rem;
+`;
+
+export const ItemContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.7rem;
+`;
--- dojang-crush/src/pages/Calendar.js
+++ dojang-crush/src/pages/Calendar.js
@@ -1,11 +1,71 @@
 import { TitleComponent } from '../components/common/header/TitleComponent';
 import { Calendar } from '../components/calendar/Calendar';
+import { useEffect, useState } from 'react';
+import { getDatePosts, getMonthlyPosts } from '../api/post';
+import Modal from 'react-modal';
+import { ModalContent } from '../components/calendar/ModalContent';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        padding: '20px',
+        borderRadius: '10px',
+        width: '20rem',
+        height: '20rem',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+};
 
 const CalendarPage = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [date, setDate] = useState('2000-02-14');
+
+    const [datePost, setDatePost] = useState([]);
+
+    useEffect(() => {
+        getDatePosts(date).then((res) => {
+            if (res && res.postList) {
+                setDatePost(res.postList);
+            }
+        });
+    }, [date]);
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    });
+
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => {
+        setIsOpen(false);
+        setDatePost([]);
+    };
+
     return (
         <>
             <TitleComponent title="Calendar" />
-            <Calendar />
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+            >
+                <ModalContent posts={datePost} />
+            </Modal>
+            <Calendar setIsOpen={setIsOpen} setDate={setDate} />
         </>
     );
 };
